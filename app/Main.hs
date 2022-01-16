c629902999ad49066814ead415b9dbbd2dbd7a1e
Jimmy Chen Chen <chenjimmy369@gmail.com>
2022-01-15 16:22:19 -0800

    Minor cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -5,7 +5,7 @@ import Text.Parsec.String (parseFromFile)
 
 -- parseProg = parse expr "Parse Error" "1_helloWorld"
 main :: IO ()
-main = do { result <- parseFromFile prog "test/program.txt"
+main = do { result <- parseFromFile prog "test/playground.txt"
           ; case result of
                 Left err  -> print err
                 Right xs  -> print xs
