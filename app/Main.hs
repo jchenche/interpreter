79483cc647eaa0f8c2ea62b8f6c5f79c5b85f108
Jimmy Chen Chen <chenjimmy369@gmail.com>
2022-01-17 09:14:42 -0800

    Unhide mtl library for the state monad

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,8 @@ module Main where
 
 import Parser.Parser
 import Text.Parsec.String (parseFromFile)
+import Control.Monad.State -- From the mtl library
+-- import Control.Monad.Trans.State -- From the transformers library
 
 -- parseProg = parse expr "Parse Error" "1_helloWorld"
 main :: IO ()
