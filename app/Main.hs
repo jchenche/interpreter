1b005621f26bc3e6f28736cf3aa21a1d97ad3262
Jimmy Chen Chen <chenjimmy369@gmail.com>
2022-01-18 21:39:35 -0800

    Getting ready for type checking

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,19 @@
 module Main where
 
+import AST.PlainAST
 import Parser.Parser
 import Text.Parsec.String (parseFromFile)
-import Control.Monad.State -- From the mtl library
-import AST.PlainAST
--- import Control.Monad.Trans.State -- From the transformers library
+import Analyzer.SemanticAnalyzer
 
 -- parseProg = parse expr "Parse Error" "1_helloWorld"
 main :: IO ()
 main = do { result <- parseFromFile prog "test/program.txt"
           ; case result of
-                Left err  -> print err
-                Right xs  -> do { print (xs == program); }
+                Left err -> print err
+                Right xs -> do
+                            { return $ typecheck xs
+                            ; print (xs == program)
+                            }
           }
 
 -- Note: during evaluation, use fromIntegral
--- src/Analyzer/SemanticAnalyzer.hs
+++ src/Analyzer/SemanticAnalyzer.hs
@@ -1,4 +1,9 @@
 module Analyzer.SemanticAnalyzer where
 
--- eval :: Expr -> State Env Val
--- eval = undefined
+import AST.PlainAST
+import qualified AST.TypedAST as T
+import Control.Monad.State -- From the mtl library
+-- import Control.Monad.Trans.State -- From the transformers library
+
+typecheck :: Prog -> State Env T.Prog
+typecheck = undefined
