e0e036263c533d7345cfe4c5532feff7662a329d
Jimmy Chen Chen <chenjimmy369@gmail.com>
2022-01-19 19:37:59 -0800

    Separate func return type from param types

--- src/AST/CommonAST.hs
+++ src/AST/CommonAST.hs
@@ -3,7 +3,7 @@ module AST.CommonAST where
 type Ident = String
 
 -- Function signature [t0, t1, ... , tn] means the function has return type t0 and parameter types t1 to tn
-data Type = TInt | TFloat | TChar | TBool | TInts | TFloats | TChars | TBools | TVoid | Sig [Type]
+data Type = TInt | TFloat | TChar | TBool | TInts | TFloats | TChars | TBools | TVoid | Sig Type [Type]
     deriving (Show, Eq)
 
 data Param = Param Type Ident
