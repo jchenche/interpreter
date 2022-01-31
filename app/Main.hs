66c8809f65d3fd16b0c9cdcdf443f0a11fcd1a27
Jimmy Chen Chen <chenjimmy369@gmail.com>
2022-01-30 21:19:09 -0800

    Formatting

--- app/Main.hs
+++ app/Main.hs
@@ -8,18 +8,22 @@ main :: IO ()
 main =
     do { result <- parseSource "test/program.txt"
        ; case result of
-             Left err  -> putStr "\x1b[31m" >> putStr "Parse error: " >> print err >> putStr "\x1b[30m"
+             Left err  ->
+                 putStr "\x1b[31m" >> putStr "Parse error: " >> print err >> putStr "\x1b[30m"
              Right ast ->
                  do { case typeCheckAST ast of
-                          (Left err, env) -> putStr "\x1b[31m" >> putStr "Semantic error: " >> print err >>
-                                             putStr "\x1b[30m" >> putStr "Environment: " >> print env
+                          (Left err, env) ->
+                              putStr "\x1b[31m" >> putStr "Semantic error: " >> print err >>
+                              putStr "\x1b[30m" >> putStr "Environment: " >> print env
                           (Right ast, _)  ->
                               do { putStr "\x1b[32m" >> putStrLn "=== BEGIN EXECUTION ===" >> putStr "\x1b[30m"
-                                   ; result <- interpretAST ast
-                                   ; case result of
-                                         (Left err, env) -> putStr "\x1b[31m" >> putStr "Runtime error: " >> print err >>
-                                                            putStr "\x1b[30m" >> putStr "Environment: " >> print env
-                                         (Right _, _)    -> putStr "\x1b[32m" >> putStrLn "=== SUCCESSFUL EXECUTION ===" >> putStr "\x1b[30m"
+                                 ; result <- interpretAST ast
+                                 ; case result of
+                                       (Left err, env) ->
+                                           putStr "\x1b[31m" >> putStr "Runtime error: " >> print err >>
+                                           putStr "\x1b[30m" >> putStr "Environment: " >> print env
+                                       (Right _, _)    ->
+                                           putStr "\x1b[32m" >> putStrLn "=== SUCCESSFUL EXECUTION ===" >> putStr "\x1b[30m"
                                  }
                     }
        }
